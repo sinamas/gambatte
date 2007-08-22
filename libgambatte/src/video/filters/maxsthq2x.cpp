b27132c52f2696837caaab4c2be73a9d27bfa94c
sinamas <sinamas@users.sourceforge.net>
2007-08-22 04:19:57 +0000

    Add copyright notice.
    git-svn-id: https://gambatte.svn.sourceforge.net/svnroot/gambatte@7 9dfb2916-2d38-0410-aef4-c5fe6c9ffc24

--- libgambatte/src/video/filters/maxsthq2x.cpp
+++ libgambatte/src/video/filters/maxsthq2x.cpp
@@ -2,6 +2,9 @@
  *   Copyright (C) 2007 by Sindre Aamås                                    *
  *   aamas@stud.ntnu.no                                                    *
  *                                                                         *
+ *   Copyright (C) 2003 MaxSt                                              *
+ *   maxst@hiend3d.com                                                     *
+ *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License version 2 as     *
  *   published by the Free Software Foundation.                            *
