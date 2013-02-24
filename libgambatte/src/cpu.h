8c6a1150bd368fe5d0926a5ef668fb980e4b6c64
sinamas <sinamas@users.sourceforge.net>
2013-02-24 22:35:06 +0100

    libgambatte/cpu: formatting/readability

--- libgambatte/src/cpu.h
+++ libgambatte/src/cpu.h
@@ -24,33 +24,12 @@
 namespace gambatte {
 
 class CPU {
-	Memory memory;
-
-	unsigned long cycleCounter_;
-
-	unsigned short PC_;
-	unsigned short SP;
-
-	unsigned HF1, HF2, ZF, CF;
-
-	unsigned char A_, B, C, D, E, /*F,*/ H, L;
-
-	bool skip;
-
-	void process(unsigned long cycles);
-
 public:
-
 	CPU();
-// 	void halt();
-
-// 	unsigned interrupt(unsigned address, unsigned cycleCounter);
-
 	long runFor(unsigned long cycles);
 	void setStatePtrs(SaveState &state);
 	void saveState(SaveState &state);
 	void loadState(const SaveState &state);
-
 	void loadSavedata() { memory.loadSavedata(); }
 	void saveSavedata() { memory.saveSavedata(); }
 
@@ -81,10 +60,8 @@ public:
 	bool loaded() const { return memory.loaded(); }
 	char const * romTitle() const { return memory.romTitle(); }
 	PakInfo const pakInfo(bool multicartCompat) const { return memory.pakInfo(multicartCompat); }
-
 	void setSoundBuffer(uint_least32_t *const buf) { memory.setSoundBuffer(buf); }
 	unsigned fillSoundBuffer() { return memory.fillSoundBuffer(cycleCounter_); }
-
 	bool isCgb() const { return memory.isCgb(); }
 
 	void setDmgPaletteColor(unsigned palNum, unsigned colorNum, unsigned rgb32) {
@@ -93,6 +70,17 @@ public:
 
 	void setGameGenie(const std::string &codes) { memory.setGameGenie(codes); }
 	void setGameShark(const std::string &codes) { memory.setGameShark(codes); }
+
+private:
+	Memory memory;
+	unsigned long cycleCounter_;
+	unsigned short PC_;
+	unsigned short SP;
+	unsigned HF1, HF2, ZF, CF;
+	unsigned char A_, B, C, D, E, /*F,*/ H, L;
+	bool skip;
+
+	void process(unsigned long cycles);
 };
 
 }
